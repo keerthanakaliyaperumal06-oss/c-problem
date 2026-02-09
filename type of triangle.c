d926d866576f033b6167058795083df8eaf908bd
keerthanakaliyaperumal06-oss <keerthanakaliyaperumal06@gmail.com>
2026-02-09 14:56:47 +0530

    Add files via upload
    type of triangle.c

---
+++ type of triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if (a == b && b== c){
+        printf("equilateral triangle");
+    }
+    else {
+        printf("not an equilateral triangle");
+    }
+    return 0;
+}
