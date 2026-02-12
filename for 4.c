e9d731bd06da2cedecf1de848933f1f9937f63b9
keerthanakaliyaperumal06-oss <keerthanakaliyaperumal06@gmail.com>
2026-02-12 09:12:40 +0530

    Add files via upload
    for 4.c

---
+++ for 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int v;
+    scanf("%d",&v);
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        printf("%d",v*i);
+        
+    }
+
+    return 0;
+}
