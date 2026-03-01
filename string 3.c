f0e5e4c3916a8f7c61a1ef1b8ff10c3d6f4d8cc2
keerthanakaliyaperumal06-oss <keerthanakaliyaperumal06@gmail.com>
2026-03-01 16:30:01 +0530

    Add files via upload

---
+++ string 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char email[100];
+    int i=0;
+    scanf("%s",&email);
+    while(email[i]!='@'){
+        printf("%c",email[i]);
+        i++;
+    }
+    return 0;
+}
+
