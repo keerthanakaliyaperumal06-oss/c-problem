cd30c164f82363c27344e9342aeb589455c6e572
keerthanakaliyaperumal06-oss <keerthanakaliyaperumal06@gmail.com>
2026-03-04 20:36:46 +0530

    Add files via upload

---
+++ for loop13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int errors;
+    int totalErrors = 0;
+    int criticalHours = 0;
+
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &errors);
+        totalErrors += errors;
+
+        if(errors > 50) {
+            criticalHours++;
+        }
+    }
+
+    printf("Total Errors: %d\n", totalErrors);
+    printf("Critical Hours: %d\n", criticalHours);
+
+    return 0;
+}
